5f37e7629868b686076fcfa16a9796dcdf72eaa9
Person-PC\Shreshtha <shreshtha.solanki@healthcoco.com>
2017-02-28 19:30:56 +0530

    HLTCOCO / Task HAPPY-1470 Added files to vcs

---
+++ HealthcocoPad/app/src/main/java/com/healthcoco/healthcocoplus/viewholders/SettingsUIPermissionItemViewHolder.java
@@ -0,0 +1,45 @@
+package com.healthcoco.healthcocoplus.viewholders;
+
+import android.support.v7.widget.AppCompatCheckBox;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.healthcoco.healthcocopad.R;
+import com.healthcoco.healthcocoplus.HealthCocoActivity;
+import com.healthcoco.healthcocoplus.HealthCocoApplication;
+import com.healthcoco.healthcocoplus.HealthCocoViewHolder;
+import com.healthcoco.healthcocoplus.enums.SettingsItemType;
+import com.healthcoco.healthcocoplus.utilities.Util;
+
+import java.util.List;
+
+/**
+ * Created by Shreshtha on 28-02-2017.
+ */
+public class SettingsUIPermissionItemViewHolder {
+    private final HealthCocoApplication mApp;
+    private Object list;
+    private HealthCocoActivity mActivity;
+    private AppCompatCheckBox chUIPermission;
+
+    public SettingsUIPermissionItemViewHolder(HealthCocoActivity mActivity) {
+        this.mActivity = mActivity;
+        this.mApp = (HealthCocoApplication) mActivity.getApplication();
+    }
+
+    public void setData(Object object) {
+        this.list = object;
+    }
+
+    public void applyData(String position) {
+        chUIPermission.setText(Util.getValidatedValueWithoutQuotes(position));
+    }
+
+    public View getContentView() {
+        LinearLayout view = (LinearLayout) mActivity.getLayoutInflater().inflate(R.layout.item_ui_permisssion, null);
+        chUIPermission = (AppCompatCheckBox) view.findViewById(R.id.ch_ui_permission);
+
+        return view;
+    }
+}
