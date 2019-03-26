a269e5593617b88525556fc329f336996f99a456
Shreshtha <shreshtha.solanki@healthcoco.com>
2019-03-26 15:33:52 +0530

    HAPPY-4273  Delete patient count issue and delete patient reflect
    (cherry picked from commit 155ccc6)

--- HealthcocoPad/HealthcocoPad/src/main/java/com/healthcoco/healthcocopad/drawer/MenuListAdapter.java
+++ HealthcocoPad/HealthcocoPad/src/main/java/com/healthcoco/healthcocopad/drawer/MenuListAdapter.java
@@ -5,17 +5,21 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 
 import com.healthcoco.healthcocopad.HealthCocoActivity;
+import com.healthcoco.healthcocopad.bean.MenuItem;
 import com.healthcoco.healthcocopad.enums.FragmentType;
+import com.healthcoco.healthcocopad.utilities.Util;
 import com.healthcoco.healthcocopad.viewholders.MenuListViewHolder;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 
 /**
  * Created by Shreshtha on 30-01-2017.
  */
 public class MenuListAdapter extends BaseAdapter {
 
-    private ArrayList<FragmentType> list;
+    private LinkedHashMap<FragmentType, MenuItem> menuItemListHashmap;
+    private ArrayList<MenuItem> list;
     private HealthCocoActivity mActivity;
     private MenuListViewHolder holder;
     private int selectedPosition = 0;
@@ -29,6 +33,8 @@ public class MenuListAdapter extends BaseAdapter {
 
     @Override
     public int getCount() {
+        if (Util.isNullOrEmptyList(list))
+            return 0;
         return list.size();
     }
 
@@ -58,15 +64,26 @@ public class MenuListAdapter extends BaseAdapter {
         return convertView;
     }
 
-    public void setListData(ArrayList<FragmentType> fragmentType) {
-        this.list = fragmentType;
+    public void setListData(ArrayList<MenuItem> menuItems) {
+        if (!Util.isNullOrEmptyList(menuItems)) {
+            menuItemListHashmap = new LinkedHashMap<>();
+            for (MenuItem menuItem :
+                    menuItems) {
+                menuItemListHashmap.put(menuItem.getFragmentType(), menuItem);
+            }
+        } else menuItemListHashmap = null;
+        this.list = menuItems;
     }
 
     public void setSetSelectedPosition(FragmentType fragmentType) {
-        if (fragmentType != FragmentType.HELP_IMPROVE && list.contains(fragmentType)) {
-            this.selectedPosition = list.indexOf(fragmentType);
-            notifyDataSetChanged();
+        if (!Util.isNullOrEmptyList(list)) {
+            if (fragmentType != FragmentType.HELP_IMPROVE && list.contains(fragmentType)) {
+                MenuItem menuItem = menuItemListHashmap.get(fragmentType);
+                if (list.contains(menuItem)) {
+                    this.selectedPosition = list.indexOf(menuItem);
+                    notifyDataSetChanged();
+                }
+            }
         }
     }
-
 }
